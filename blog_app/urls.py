efa96f62ead76f354a44f1e87e839ddb314aa23f
Isaiah-Ambi <ambiisaiah24@gmail.com>
2023-10-01 22:24:30 +0100

    added comments and pagination

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,7 +1,10 @@
 from . import views
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-]
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
